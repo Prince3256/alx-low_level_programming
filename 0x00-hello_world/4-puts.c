627a9c6f6765e3f18cd04af5513129f4b4ce8e5e
Prince3256 <sollyprince7@gmail.com>
2022-12-12 06:01:24 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Return: Alwatys 0 (success)
+ */
+int main(void)
+
+{
+	puts ("\"programming is like building a multilinqual puzzle");
+	return (0);
+	
+}
