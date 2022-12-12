7541fd0b4b8693f024a0b176d651d7a330a0dd1c
Prince3256 <sollyprince7@gmail.com>
2022-12-12 10:14:06 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
+ * main - Entry Point 
  *
- * Return: Alwatys 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("\"programming is like building a multilinguan puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
