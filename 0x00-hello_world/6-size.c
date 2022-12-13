b7389eaaba902c54d2079ed0b8f08a39331d6757
Prince3256 <sollyprince7@gmail.com>
2022-12-13 06:03:33 -0800

    C program that prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints sized of characters
+ * main - Entry Point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+
+	char c;
+	int d;
+	long int ld;
+	long long int lld;
+	float f;
+
+	printf("size of a char: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("size of an int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(ld));
+	printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(lld));
+	printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
+
 }
