a34fb8375664061d47db71f1ddcf438cb66d54ea
Prince3256 <sollyprince7@gmail.com>
2022-12-19 07:25:34 -0800

    Numberz and Letters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,17 +11,19 @@ int main(void)
 	char c;
 
 	int d;
+
 	c = 'a';
 	d = 0;
 	while
 		(d < 10) {
-			putchar(d +'0');
+			putchar(d + '0');
 			d++;
 		}
 	while
 		(c <= 'f') {
 			putchar(c);
 			c++;
+
 		}
 	putchar('\n');
 	return (0);
